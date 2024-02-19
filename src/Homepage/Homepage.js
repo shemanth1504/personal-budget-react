7b32c5e74f07e5a87c565c2d30a6463e8cef0728
Sai Hemanth Mallam <smallam1@uncc.edu>
2024-02-18 20:11:52 -0500

    Brought pbudget code in

---
+++ src/Homepage/Homepage.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <main role="main" class="center" >
+    <div class="page-area">
+     <article role="article">
+            <h1>Stay on track</h1>
+            <p>
+                Do you know where you are spending your money? If you really stop to track it down,
+                you would get surprised! Proper budget management depends on real data... and this
+                app will help you with that!
+            </p>
+        </article>
+
+        <article>
+            <h1>Alerts</h1>
+            <p>
+                What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
+            </p>
+        </article>
+
+        <article>
+            <h1>Results</h1>
+            <p>
+                People who stick to a financial plan, budgeting every expense, get out of debt faster!
+                Also, they to live happier lives... since they expend without guilt or fear... 
+                because they know it is all good and accounted for.
+            </p>
+        </article>
+
+        <article>
+            <h1>Chart</h1>
+            <canvas id="myChart" width="450" height="450"></canvas>
+        </article>
+
+        <article>
+            <h1>Stay on track</h1>
+            <p>
+                Do you know where you are spending your money? If you really stop to track it down,
+                you would get surprised! Proper budget management depends on real data... and this
+                app will help you with that!
+            </p>
+        </article>
+
+        <article>
+            <h1>Alerts</h1>
+            <p>
+                What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
+            </p>
+        </article>
+
+        <article>
+            <h1>Results</h1>
+            <p>
+                People who stick to a financial plan, budgeting every expense, get out of debt faster!
+                Also, they to live happier lives... since they expend without guilt or fear... 
+                because they know it is all good and accounted for.
+            </p>
+        </article>
+
+        <article>
+            <h1>Free</h1>
+            <p>
+                This app is free!!! And you are the only one holding your data!
+            </p>
+        </article>
+
+        <article>
+            <h1>D3.js Chart</h1>
+            <button class="randomize">randomize</button>
+
+            <div id="d3-chart-container">
+            </div>
+        </article>
+
+    </div>
+
+</main>
+
+  );
+}
+
+export default HomePage;
+    
